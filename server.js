d3739de496ec1e261c2fd1889301a27ad6537eda
Trainer-Blue <ishansiddhartha@gmail.com>
2026-02-10 22:31:20 +0530

    fix: remove localhost from cors 💀

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 // Enable CORS for frontend
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
